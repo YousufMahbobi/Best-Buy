c4661b6cab040e528568029b20e8b4ad78799df0
Sayed Yousuf Mahbobi <rayanguzman345@gmail.com>
2025-07-06 02:02:13 +0200

    Store logic implemented

--- products.py
+++ products.py
@@ -1,16 +1,18 @@
-import validation
 from validation import Validation
 
 class Product:
     def __init__(self, name, price, quantity):
-        Validation.validate_name(name)
-        Validation.validate_price(price)
-        Validation.validate_quantity(quantity)
+        try:
+            Validation.validate_name(name)
+            Validation.validate_price(price)
+            Validation.validate_quantity(quantity)
 
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.active = True
+            self.name = name
+            self.price = price
+            self.quantity = quantity
+            self.active = True
+        except Exception as e:
+            print(e)
 
 
     def get_quantity(self):
@@ -19,7 +21,7 @@ class Product:
 
     def set_quantity(self, quantity):
         Validation.validate_quantity(quantity)
-        self.quantity += quantity
+        self.quantity = quantity + self.get_quantity()
         self.check_and_deactivate_product_out_of_stock()
 
 
@@ -37,7 +39,7 @@ class Product:
         Validation.validate_quantity(quantity)
         if self.is_active():
             Validation.validate_quantity_available(self.quantity, quantity)
-            self.quantity -= quantity
+            self.quantity = self.get_quantity() - quantity
             self.check_and_deactivate_product_out_of_stock()
             return quantity * self.price
         return 0
@@ -45,19 +47,3 @@ class Product:
     def check_and_deactivate_product_out_of_stock(self):
         if self.quantity == 0:
             self.deactivate()
-
-
-
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
-
-
-bose.show()
-mac.show()
-
-bose.set_quantity(1000)
-bose.show()
--- validation.py
+++ validation.py
@@ -1,23 +1,38 @@
 class Validation:
+
     @staticmethod
     def validate_name(name:str):
+        if not isinstance(name, str):
+            raise TypeError('Product name must be a string')
         if not name:
-            raise ValueError('Name cannot be empty')
+            raise ValueError('Product name cannot be empty')
+
 
     @staticmethod
-    def validate_price(price:float):
-        if price < 0:
-            raise ValueError('Price cannot be negative or zero')
+    def validate_price(price):
+        if not isinstance(price, (float, int)):
+            raise ValueError('Product price must be a float')
+
+        if price < 0.0:
+            raise ValueError('Product price cannot be negative or zero')
+
 
     @staticmethod
-    def validate_quantity(quantity:int):
+    def validate_quantity(quantity):
+        if not isinstance(quantity, int):
+            raise ValueError('Product quantity must be a int')
+
         if quantity < 0:
-            raise ValueError('Quantity cannot be negative or zero')
+            raise ValueError('Product quantity cannot be negative or zero')
+
 
     @staticmethod
     def validate_quantity_available(available_quantity:int, requested_quantity:int):
         if requested_quantity > available_quantity:
             raise ValueError('Requested quantity cannot be greater than available quantity')
+
+
+
 
 
 
